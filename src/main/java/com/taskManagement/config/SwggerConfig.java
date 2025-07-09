d7f8c172151c71521844ba0c240201a8eca1be56
AayushKDIPL <aayush@kanakdrishtiinfo.com>
2025-07-09 12:57:37 +0530

    Update SwggerConfig.java

--- src/main/java/com/taskManagement/config/SwggerConfig.java
+++ src/main/java/com/taskManagement/config/SwggerConfig.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwggerConfig {
@@ -16,8 +17,8 @@ public class SwggerConfig {
 	OpenAPI customOpenAPI() {
 		return new OpenAPI().addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
 				.components(new Components().addSecuritySchemes("Bearer Authentication", createAPTScheme()))
-				.info(new Info().title("Master Schema API").description("This is for global use"));
-//				.addServersItem(new Server().url("https://app.ventureconsultancyservices.com/taskManagement"));
+				.info(new Info().title("Master Schema API").description("This is for global use"))
+				.addServersItem(new Server().url("https://indkisandairy.com/taskManagement"));
 	}
 
 	public SecurityScheme createAPTScheme() {
